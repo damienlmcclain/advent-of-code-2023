dbcfdcc01f24d30fdbf79f40f596cdf458b77267
dmcclain <dmcclain@ginkgobioworks.com>
2023-12-07 07:13:42 -0500

    Use AOCD and refactor day 1 functions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# advent-of-code-2023
+My Advent of Code 2023 Repository!
+
+To pull data automatically from the AOC website, follow the instructions 
+(here)[https://github.com/wimglenn/advent-of-code-wim/issues/1]. Then locally create a file called session_cookie.py
+and add your session cookie to it as a str variable called AOCD_SESSION_COOKIE.
--- aoc2023/day01.py
+++ aoc2023/day01.py
@@ -1,22 +1,57 @@
+import aocd
+import os
 import re
 
 
-def part_one_find_ints(filename):
+def input_parser(day=None, year=None, session_cookie=None, filename=None):
     """
     Finds the ints in each line and returns them as a list of lists.
 
     Parameters
     ----------
+    day: int
+        The day to pull data for.
+    year: int
+        The year to pull data for.
+    session_cookie: str
+        The AOC session cookie for pulling data.
     filename: str
-        The name of the file to parse.
+        If input, the name of the file to parse instead of using day/year.
+
+    Returns
+    -------
+    list of str
+        A list of each line in the pulled data or file as a str.
+    """
+    if filename:
+        with open(filename, encoding='utf-8') as file:
+            input_data = file.read().split('\n')
+    elif day and year and session_cookie:
+        os.environ['AOC_SESSION'] = session_cookie
+        data = aocd.get_data(day=day, year=year)
+        input_data = data.split('\n')
+    else:
+        return 'Please enter a valid combination of inputs: either a ' \
+               'filename to pull from locally or a day, year, and ' \
+               'session_cookie.'
+    return input_data
+
+
+def part_one_find_ints(input_data):
+    """
+    Finds the calibration value ints in each line and returns them as a list
+    of lists.
+
+    Parameters
+    ----------
+    input_data: list of str
+        The list of calibration values to parse.
 
     Returns
     -------
     list of lists
-        A list of lists containing the ints on each line.
+        A list of lists containing the calibration value ints on each line.
     """
-    with open(filename, encoding='utf-8') as file:
-        input_data = file.read().split('\n')
     all_nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     all_line_ints = []
     for line in input_data:
@@ -25,22 +60,21 @@ def part_one_find_ints(filename):
     return all_line_ints
 
 
-def part_two_find_nums(filename):
+def part_two_find_nums(input_data):
     """
-    Finds the numbers in each line, either as ints or written out, and returns them as a list of lists.
+    Finds the numbers in each line, either as ints or written out as strs,
+    and returns them as a list of lists.
 
     Parameters
     ----------
-    filename: str
-        The name of the file to parse.
+    input_data: list of str
+        The list of calibration values to parse.
 
     Returns
     -------
     list of lists
         A list of lists containing the ints on each line.
     """
-    with open(filename, encoding='utf-8') as file:
-        input_data = file.read().split('\n')
     all_ints = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     all_nums_as_strs = {
         'zero': 0,
@@ -56,9 +90,7 @@ def part_two_find_nums(filename):
     }
     all_line_ints = []
     for line in input_data:
-        str_num = ''
         line_nums = {}
-        altered_line = line
         line_index = 0
         for character in line:
             if character in all_ints:
@@ -75,22 +107,55 @@ def part_two_find_nums(filename):
     return all_line_ints
 
 
-def find_calibration_value(calibration_values_list):
+def find_calibration_value(part,
+                           day=None,
+                           year=None,
+                           session_cookie=None,
+                           filename=None):
     """
-    Given a list of lists of values, finds the original elf calibration values and sums them.
+    Given a list of lists of values, finds the original elf calibration values
+    and sums them.
 
     Parameters
     ----------
-    calibration_values_list: list of lists
-        The list of art-ified calibration values.
+    part: int
+        Part one or two for the day. Determines how the data is parsed.
+    day: int
+        The day to pull data for.
+    year: int
+        The year to pull data for.
+    session_cookie: str
+        The AOC session cookie for pulling data.
+    filename: str
+        If input, the name of the file to parse instead of using day/year.
 
     Returns
     ----------
     int
         The sum of the correct calibration values.
     """
+    if filename:
+        massaged_input_data = input_parser(filename=filename)
+    elif day and year and session_cookie:
+        massaged_input_data = input_parser(day=day,
+                                           year=year,
+                                           session_cookie=session_cookie)
+    elif not day or not year:
+        return 'You have not entered either a filename or a day and year ' \
+               'combination. Please enter one of the two and retry.'
+    elif not session_cookie:
+        return 'You have not entered either a filename or a session cookie. ' \
+               'Please enter one of the two and retry.'
+
+    if part == 1:
+        nums_to_parse = part_one_find_ints(input_data=massaged_input_data)
+    elif part == 2:
+        nums_to_parse = part_two_find_nums(input_data=massaged_input_data)
+    else:
+        return f'Please enter 1 or 2 as the part. {part} is not a valid part.'
+
     calibration_value_sum = 0
-    for line in calibration_values_list:
+    for line in nums_to_parse:
         calibration_value = 0
         if len(line) == 1:
             calibration_value = line[0] * 10 + line[0]
