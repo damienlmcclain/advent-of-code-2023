b25ec430f6fd2495223a0c040bb91deb50962350
Damien McClain <122814213+damienlmcclain@users.noreply.github.com>
2025-01-09 16:31:57 -0500

    Add day 2 part 2

--- aoc2023/tests/test_day02.py
+++ aoc2023/tests/test_day02.py
@@ -1,14 +1,25 @@
 from unittest import TestCase
 
-from aoc2023.day02 import determine_possible_games, parse_input
+from aoc2023.day02 import create_cube_dict, determine_possible_games, parse_input
 from aoc2023.session_cookie import AOCD_SESSION_COOKIE
 
 
 class Test(TestCase):
     max_allowed_colors = {'red': 12, 'green': 13, 'blue': 14}
+    parsed_easy_input = [
+        'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',
+        'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue',
+        'Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red',
+        'Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red',
+        'Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
+    ]
 
     def test_parse_input(self):
         res = parse_input(filename='tests/fixtures/day_02_easy_input.txt')
+        self.assertEqual(res, self.parsed_easy_input)
+
+    def test_create_cube_dict(self):
+        res = create_cube_dict(input_data=self.parsed_easy_input)
         expected_res = {
             1: {
                 'blue': 6,
@@ -38,16 +49,34 @@ class Test(TestCase):
         }
         self.assertEqual(res, expected_res)
 
-    def test_determine_possible_games(self):
+    def test_determine_possible_games_sum(self):
         res = determine_possible_games(
             max_of_each_color=self.max_allowed_colors,
+            sum_or_product='sum',
             filename='tests/fixtures/day_02_easy_input.txt')
         self.assertEqual(res, 8)
 
-    def test_determine_possible_games_full_input(self):
+    def test_determine_possible_games_sum_full_input(self):
         res = determine_possible_games(
             max_of_each_color=self.max_allowed_colors,
+            sum_or_product='sum',
             day=2,
             year=2023,
             session_cookie=AOCD_SESSION_COOKIE)
         self.assertEqual(res, 2679)
+
+    def test_determine_possible_games_product(self):
+        res = determine_possible_games(
+            max_of_each_color=self.max_allowed_colors,
+            sum_or_product='product',
+            filename='tests/fixtures/day_02_easy_input.txt')
+        self.assertEqual(res, 2286)
+
+    def test_determine_possible_games_product_full_input(self):
+        res = determine_possible_games(
+            max_of_each_color=self.max_allowed_colors,
+            sum_or_product='product',
+            day=2,
+            year=2023,
+            session_cookie=AOCD_SESSION_COOKIE)
+        self.assertEqual(res, 77607)
